0b2d88671b1c49638122aa12bde88aff177123ef
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2025-07-29 23:06:06 -0700

    feat(allocate_resources-handler): implement async handler with multi-stage reply for resource allocation

--- app/command_handlers/handlers/allocate_resources.py
+++ app/command_handlers/handlers/allocate_resources.py
@@ -1,10 +1,11 @@
 import asyncio
-from app.redis_utils.decorators import multi_stage_reply
 import logging
+
+from app.redis_utils.decorators import multi_stage_reply
 
 
 STREAM_NAME = "resources:commands"
-GROUP_NAME = "resources_handler_group"
+GROUP_NAME = "resources_allocator_group"
 EVENT_TYPE = "resources:allocate"
 
 logger = logging.getLogger(__name__)
@@ -16,5 +17,6 @@ async def handle(fields: dict, progress) -> None:
     Handle allocate_resources command.
     """
     logger.info("Handling allocate_resources command")
+    print("Handling allocate_resources command")
     await progress(0.5, {"stage": "allocating"})
     await asyncio.sleep(1)
