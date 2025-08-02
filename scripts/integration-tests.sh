ea963e991d4e1a66b3fc9a491f6052cc7f941de9
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2025-08-01 23:14:46 -0700

    chore(infra): update docker-compose files and integration test script

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -2,5 +2,7 @@
 set -e
 
 # podman-compose -f docker-compose.yml -f docker-compose.integration.yaml down -v || true
-podman-compose -f docker-compose.yml -f docker-compose.integration.yaml \
-  up --build --exit-code-from integration_test integration_test
+podman-compose -f docker-compose.yml up -d
+podman-compose -f docker-compose.yml -f docker-compose.integration.yaml run --rm integration_test
+STATUS=$?
+exit $STATUS
